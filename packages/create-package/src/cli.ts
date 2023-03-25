01807188780bd09b9e50373104a4b18077f9162c
skulptur <g.spredemann@gmail.com>
2023-03-26 00:21:26 +0100

    use enquirer to get missing options

--- packages/create-package/src/cli.ts
+++ packages/create-package/src/cli.ts
@@ -2,6 +2,9 @@
 
 import { program } from "commander";
 import { copyTemplateAndInstall } from "./index";
+import Enquirer from "enquirer";
+import * as path from "path";
+import * as fs from "fs-extra";
 
 // Parse command line arguments
 program
@@ -12,8 +15,55 @@ program
 
 const options = program.opts();
 
-copyTemplateAndInstall({
-  templateDir: options.templateDir,
-  targetDir: options.targetDir,
-  libraryName: options.libraryName,
-});
+// Use Enquirer to prompt the user for the necessary information if the options are not provided
+async function promptUser() {
+  const enquirer = new Enquirer();
+
+  const questions = [];
+  if (!options.templateDir) {
+    const templateDirs = fs
+      .readdirSync("./templates", { withFileTypes: true })
+      .filter((dir) => dir.isDirectory())
+      .map((dir) => dir.name);
+    questions.push({
+      type: "select",
+      name: "templateDir",
+      message: "Select the template directory:",
+      choices: templateDirs,
+      initial: 0,
+    });
+  }
+  if (!options.libraryName) {
+    questions.push({
+      type: "input",
+      name: "libraryName",
+      message: "Enter the new library name:",
+    });
+    if (!options.targetDir) {
+      questions.push({
+        type: "input",
+        name: "targetDir",
+        message: "Enter the target directory path:",
+        initial: path.join(
+          process.cwd(),
+          "../",
+          // @ts-ignore
+          options.name || answers.libraryName
+        ),
+      });
+    }
+  }
+  const answers = await enquirer.prompt(questions);
+  const mergedOptions = { ...options, ...answers };
+  return mergedOptions;
+}
+
+// Call the copyTemplateAndInstall function with the options
+(async () => {
+  const mergedOptions = await promptUser();
+  copyTemplateAndInstall({
+    templateDir: path.join("./templates", mergedOptions.templateDir),
+    targetDir: mergedOptions.targetDir,
+    libraryName: mergedOptions.libraryName,
+  });
+})();
