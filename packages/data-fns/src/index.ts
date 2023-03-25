28acd4e449dba1e9a8e5541f7dc7e1b64637e7ba
skulptur <g.spredemann@gmail.com>
2023-03-25 19:13:07 +0100

    add data-fns

---
+++ packages/data-fns/src/index.ts
@@ -0,0 +1,19 @@
+export * from './lib/times'
+export * from './lib/generateSequence'
+
+export * from './lib/mapAt'
+export * from './lib/getItem'
+
+export * from './lib/cyclic'
+export * from './lib/palindrome'
+export * from './lib/modulo'
+
+export * from './lib/cellularAutomata'
+export * from './lib/euclideanSequencer'
+export * from './lib/euclideanSilences'
+export { markovSequence } from './lib/markovChain'
+
+export * from './lib/patternChunks'
+
+export * from './lib/binaryToIndices'
+export * from './lib/indicesToBinary'
