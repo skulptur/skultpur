9a2b5d9d1a5868c1f239714ab7e9338ef816213d
skulptur <g.spredemann@gmail.com>
2023-03-26 12:36:06 +0200

    Update getImageChannelCount.ts

--- packages/color-supreme/src/lib/getImageChannelCount.ts
+++ packages/color-supreme/src/lib/getImageChannelCount.ts
@@ -1,15 +1,17 @@
+import { ChannelCount } from './types'
+
 export function getImageChannelCount(
   imageData: Uint8ClampedArray,
   width: number,
   height: number
-): number | null {
+): ChannelCount | null {
   const totalPixels = width * height
   const totalValues = imageData.length
 
   const channelCount = totalValues / totalPixels
 
   if (channelCount >= 1 && channelCount <= 4) {
-    return channelCount
+    return channelCount as ChannelCount
   } else {
     // Return a default value (e.g., 0) when the channel count is outside the valid range.
     return null
