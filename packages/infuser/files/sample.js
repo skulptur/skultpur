3bf5242828173b68681cfd53f77b9e1be6bbea54
skulptur <g.spredemann@gmail.com>
2023-03-31 09:16:24 +0200

    add comments

--- packages/infuser/files/sample.js
+++
@@ -1,7 +0,0 @@
-/* infuser start header */
-const header = document.querySelector('header');
-/* infuser end header */
-
-/* infuser start footer */
-const footer = document.querySelector('footer');
-/* infuser end footer */
--- packages/infuser/src/lib/lib.ts
+++ packages/infuser/src/lib/lib.ts
@@ -1,8 +1,16 @@
 import { readFile as fsReadFile, writeFile as fsWriteFile } from "fs/promises";
 import { extname } from "path";
 
+/**
+The style of comment delimiters for identifying slots in a file
+*/
 type CommentStyle = { start: string; end: string };
 
+/**
+Returns the default comment style for a given file extension
+@param fileExtension - The extension of the file to check for a default comment style
+@returns The default comment style for the given file extension, or null if none exists
+*/
 export function getDefaultCommentStyle(
   fileExtension: string
 ): CommentStyle | null {
@@ -30,6 +38,14 @@ export function getDefaultCommentStyle(
   return commentStyles[fileExtension] || null;
 }
 
+/**
+Gets the start and end indices of a named slot in a file, identified by the given comment style
+@param fileContent - The content of the file to search for the named slot
+@param slotName - The name of the slot to search for
+@param commentStyle - The style of comments used to identify the slots
+@returns An object containing the start and end indices of the named slot, as well as the start and end comment delimiters
+@throws An error if the named slot cannot be found in the file
+*/
 export function getSlotIndices(
   fileContent: string,
   slotName: string,
@@ -56,6 +72,15 @@ export function getSlotIndices(
   };
 }
 
+/**
+Replaces the content of a named slot in a file, identified by the given comment style
+@param fileContent - The content of the file to modify
+@param slotName - The name of the slot to replace the content of
+@param newContent - The new content to replace the slot's old content with
+@param commentStyle - The style of comments used to identify the slots
+@returns The modified content of the file
+@throws An error if the named slot cannot be found in the file
+*/
 export function replaceContentInString(
   fileContent: string,
   slotName: string,
@@ -75,6 +100,13 @@ export function replaceContentInString(
   return `${beforeContent}${startComment}${newContent}${endComment}${afterContent}`;
 }
 
+/**
+Reads the content of a file and returns it, along with the comment style used to identify slots in the file
+@param filePath - The path to the file to read
+@param commentStyle - (optional) The style of comments used to identify slots in the file; if not provided, the default comment style for the file extension will be used
+@returns An object containing the content of the file and the comment style used to identify slots in the file
+@throws An error if no default comment style is available for the file extension and no comment style is provided
+*/
 export async function readFile(filePath: string, commentStyle?: CommentStyle) {
   commentStyle = commentStyle || getDefaultCommentStyle(extname(filePath));
   if (!commentStyle) {
@@ -96,6 +128,12 @@ function escapeRegExp(string: string): string {
   return string.replace(/[.*+\-?^${}()|[\]\\]/g, "\\$&");
 }
 
+/**
+Gets the names of all the slots in a file, identified by the given comment style
+@param fileContent - The content of the file to search for slots
+@param commentStyle - The style of comments used to identify the slots
+@returns An array of the names of all the slots in the file
+*/
 export function getSlotNames(
   fileContent: string,
   commentStyle: CommentStyle
@@ -116,6 +154,13 @@ export function getSlotNames(
   return slotNames;
 }
 
+/**
+Gets the content of all the named slots in a file, identified by the given comment style
+@param fileContent - The content of the file to search for slots
+@param slotNames - (optional) An array of the names of the slots to get; if not provided, all slots in the file will be returned
+@param commentStyle - The style of comments used to identify the slots
+@returns An array of objects containing the name and content of each named slot in the file
+*/
 export function getSlots(
   fileContent: string,
   slotNames: string[],
@@ -139,6 +184,14 @@ export function getSlots(
   return slots;
 }
 
+/**
+Updates the content of one or more named slots in a file, identified by the given comment style
+@param fileContent - The content of the file to modify
+@param updates - An array of objects containing the name and new content of each named slot to update
+@param commentStyle - The style of comments used to identify the slots
+@returns The modified content of the file
+@throws An error if the named slot cannot be found in the file
+*/
 export function updateSlots(
   fileContent: string,
   updates: Array<{ slotName: string; newContent: string }>,
@@ -161,6 +214,14 @@ export function updateSlots(
   return fileContent;
 }
 
+/**
+Updates the content of one or more named slots (identified by the given comment style) in a file and writes the changes to disk
+@param filePath - The path to the file to modify
+@param updates - An array of objects containing the name and new content of each named slot to update
+@param commentStyle - (optional) The style of comments used to identify the slots; if not provided, the default comment style for the file extension will be used
+@returns A promise that resolves when the file has been successfully updated
+@throws An error if the named slot cannot be found in the file or no default comment style is available for the file extension and no comment style is provided
+*/
 export async function updateFile(
   filePath: string,
   updates: Array<{ slotName: string; newContent: string }>,
